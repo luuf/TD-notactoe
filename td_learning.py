96401759598b9d9ff303f4cd31535d071d37afb5
luuf <lukas.finnveden@hotmail.se>
2016-10-15 17:20:22 +0200

    skit funkar!
    buggen löstes av att byta ut multiplikation mot forloopar i skapandet
    av databasen!

--- td_learning.py
+++ td_learning.py
@@ -10,7 +10,7 @@ def getdata():
 def storedata():
     pickle.dump(db,open("db.dat","wb"),pickle.HIGHEST_PROTOCOL)
 def resetdata():
-    return [[[0.5]*47]*47]*47               
+    return [[[0.5 for i in range(47)] for j in range(47)] for k in range(47)]              
 def pullstate(state):
     return db[state[0]][state[1]][state[2]]
 def setstate(state,value):
@@ -38,12 +38,11 @@ def game_win(win):
         previous_score = 0
     for state in reversed(playedmoves):
         v = pullstate(state)
-        previous_score = v+l*(previous_score-v)
+        previous_score = v+l*(previous_score - v)
         setstate(state,previous_score)
     playedmoves = []
-    print(win, db[12][12][12])
     
 def end():
     storedata()
 db = getdata() #db[0][0][0] ger värdet för state(0,0,0)
-db=resetdata()
+db = resetdata()
