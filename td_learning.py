5b68f4aa36058b36b0bd9401a4e5548c95d06b42
HjalmarWijk <hjalmar.wijk@gmail.com>
2016-10-15 18:17:33 +0200

    Final policy tillagd
    Lade  till en finalpolicy som inte tränar utan bara väljder
    argmax(states)

--- td_learning.py
+++ td_learning.py
@@ -3,10 +3,14 @@ import pickle
 import math
 import random
 l = 0.5
-temp = 0.1
+temp = 0.2
 playedmoves = []
+finalpolicy = False
 def getdata():
-    return pickle.load(open("db.dat","rb"))
+    try:
+        return pickle.load(open("db.dat","rb"))
+    except:
+        return resetdata()
 def storedata():
     pickle.dump(db,open("db.dat","wb"),pickle.HIGHEST_PROTOCOL)
 def resetdata():
@@ -17,30 +21,34 @@ def setstate(state,value):
     db[state[0]][state[1]][state[2]] = value
     
 def choice(states):
-    global playedmoves
-    scores = []
-    for state in states:
-        scores.append((math.e)**(pullstate(state)/temp))
-    sumscores = sum(scores)
-    num = random.random()
-    for index,score in enumerate(scores):
-        if score/sumscores < num:
-            num = num - score/sumscores
-        else:
-            playedmoves.append(states[index])
-            return states[index]
+    if finalpolicy:
+        return max(states, key=lambda x:db[x[0]][x[1]][x[2]])
+    else:
+        global playedmoves
+        scores = []
+        for state in states:
+            scores.append((math.e)**(pullstate(state)/temp))
+        sumscores = sum(scores)
+        num = random.random()
+        for index,score in enumerate(scores):
+            if score/sumscores < num:
+                num = num - score/sumscores
+            else:
+                playedmoves.append(states[index])
+                return states[index]
 
 def game_win(win):
-    global playedmoves
-    if win:
-        previous_score = 1
-    else:
-        previous_score = 0
-    for state in reversed(playedmoves):
-        v = pullstate(state)
-        previous_score = v+l*(previous_score - v)
-        setstate(state,previous_score)
-    playedmoves = []
+    if not finalpolicy:
+        global playedmoves
+        if win:
+            previous_score = 1
+        else:
+            previous_score = 0
+        for state in reversed(playedmoves):
+            v = pullstate(state)
+            previous_score = v+l*(previous_score - v)
+            setstate(state,previous_score)
+        playedmoves = []
     
 def end():
     storedata()
