d0e3486383c14e68b5f1802f463740fccdfe565a
HjalmarWijk <hjalmar.wijk@gmail.com>
2016-10-20 11:32:34 +0200

    Human Player Input control
    istället för att direkt plocka input används nu en “get_input()”
    funktion, som kollar att värdet är ok. Den kan hantera typ alla inputs
    tror jag, och om det inte är korrekt formaterat printar den en varning
    och försöker igen
    
    Dessutom har jag gett lite extra spacing och information för human
    playern, förbättrat strängarna lite etc.

--- human_player.py
+++ human_player.py
@@ -1,15 +1,17 @@
 '''Human Player'''
-import random
 import database_creator_x_2000 as databaser
 db = databaser.generate_db()
 current_boards = [[[0,0,0],[0,0,0],[0,0,0]],
-          [[0,0,0],[0,0,0],[0,0,0]],
-          [[0,0,0],[0,0,0],[0,0,0]]]
-def boards_to_state(board):
+                 [[0,0,0],[0,0,0],[0,0,0]],
+                 [[0,0,0],[0,0,0],[0,0,0]]]
+def boards_to_state(board,sort = True):
     state = []
     for i in range(3):
         state.append(databaser.find_board_id([board[0][i],board[1][i],board[2][i]],db))
-    return tuple(sorted(state))
+    if sort == True:
+        return tuple(sorted(state))
+    else:
+        return tuple(state)
 def state_to_board(state,boards):
     if state == (0,0,0):
         return boards
@@ -34,14 +36,36 @@ def printboards(boards):
             string += " "
         string += "\n"
     print(string)
-                
+def get_input():
+    not_correct = True
+    while not_correct:
+        i = input("Välj var du vil spela, i formatet: bräde,rad,kolumn")
+        i = i.split(',')
+        if str(0) in i:
+            print("0 är inte en giltig siffra, välj 1,2 eller 3")
+            continue
+        try:
+            if current_boards[int(i[0])-1][int(i[1])-1][int(i[2])-1] == 0 and boards_to_state(current_boards,sort = False)[int(i[0])-1] != 12:
+                not_correct = False
+            else:
+                print("Plats ej giltig, försök igen") 
+        except (TypeError, ValueError):
+            print("Felaktig formattering - det ska vara ex. 1,1,3 för bräde 1, rad 1, kolumn 3 - försök igen. ")
+        except IndexError:
+            print("Felaktig siffra. Det finns endast 3 bräden, rader och kolumner. Välj 1,2 eller 3 för varje")
+    return i
+    
 def choice(lista,current):
     global current_boards
     current_boards = state_to_board(current,current_boards)
+    if current != (0,0,0):
+        print("\nMotståndarens drag:")
+    else:
+        print("\nBrädets utgångsposition:")
     printboards(current_boards)
-    i = input("bräde,rad,kolumn")
-    i = i.split(',')
+    i = get_input()
     current_boards[int(i[1])-1][int(i[0])-1][int(i[2])-1] = 1
+    print("\nDitt drag var:")
     printboards(current_boards)
     return boards_to_state(current_boards)
 def end():
